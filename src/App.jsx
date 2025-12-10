0fe6375f2962130dacacdb08fd9b5b0ff5a7942a
Mykola Ustymenko <ipz231_umo@student.ztu.edu.ua>
2025-12-11 00:31:46 +0200

    changed add.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import React, { useState } from "react";
+
+import Header from "./components/Header";
 import Start from "./page/Start";
+import Game from "./page/Game";
+import Res from "./page/Res";
 import "./style.css";
 
 export default function App() {
-  return (
-    <div>
-      <Start onStart={() => {}} />
-    </div>
+  const[page, setPage] = useState("start");
+
+  return(
+    <>
+      <Header />
+
+      {page === "start" && <Start onStart={() => setPage("game")} />}
+      {page === "game" && <Game onFinish={() => setPage("res")} />}
+      {page === "res" && <Res onRestart={() => setPage("start")} />}  
+    </>
   );
 }
