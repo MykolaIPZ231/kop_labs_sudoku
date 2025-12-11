add353023461f1be7373181a070f1dc309b82583
Mykola Ustymenko <ipz231_umo@student.ztu.edu.ua>
2025-12-11 17:49:10 +0200

    changed game page

--- src/page/Game.jsx
+++ src/page/Game.jsx
@@ -3,13 +3,27 @@ import React from "react";
 import SudokuG from "../components/SudokuG";
 import Button from "../components/Button";
 
-export default function Game({onFinish}) {
-    return(
+export default function Game({
+    grid,
+    selectedCell,
+    selectCell,
+    setCellValue,
+    onFinish
+}) {
+    return (
         <div className="container">
-            <SudokuG />
+            <SudokuG
+                grid={grid}
+                selectedCell={selectedCell}
+                selectCell={selectCell}
+            />
 
             <div style={{ marginTop: "20px" }}>
-                <Button onClick={onFinish}>finish</Button>
+                <Button onClick={() => setCellValue(1)}>1</Button>
+            </div>
+
+            <div style={{ marginTop: "20px" }}>
+                <Button onClick={onFinish}>Finish Game</Button>
             </div>
         </div>
     );
